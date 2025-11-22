6ada1850668969e7abc20bfb9977b6abe69c779e
RenHeyzer <vesolyyradin@gmail.com>
2025-11-22 22:03:06 +0600

    add manual DI class AppContainer

--- app/src/main/java/com/azim/fitness/FitnessTrackerApp.kt
+++ app/src/main/java/com/azim/fitness/FitnessTrackerApp.kt
@@ -1,10 +1,14 @@
 package com.azim.fitness
 
 import android.app.Application
+import com.azim.fitness.di.AppContainer
 
 class FitnessTrackerApp : Application() {
+
+    lateinit var container: AppContainer
+
     override fun onCreate() {
         super.onCreate()
-
+        container = AppContainer(applicationContext)
     }
 }
---
+++ app/src/main/java/com/azim/fitness/di/AppContainer.kt
@@ -0,0 +1,15 @@
+package com.azim.fitness.di
+
+import android.content.Context
+import androidx.room.Room
+import com.azim.fitness.db.FTDatabase
+
+class AppContainer(context: Context) {
+    private val ftDatabase: FTDatabase = Room.databaseBuilder(
+        context = context.applicationContext,
+        FTDatabase::class.java,
+        FTDatabase.DATABASE_NAME
+    ).build()
+
+    val userDao = ftDatabase.profileDao
+}
