d4d3b9e65df49307dd21b9d70b2ad382414095d5
RenHeyzer <vesolyyradin@gmail.com>
2025-11-26 22:07:43 +0600

    add class to merge and store validate messages

---
+++ app/src/main/java/com/azim/fitness/ui/registration/Messages.kt
@@ -0,0 +1,11 @@
+package com.azim.fitness.ui.registration
+
+data class Messages(
+    val lastname: String? = null,
+    val firstname: String? = null,
+    val email: String? = null,
+    val age: String? = null,
+    val height: String? = null,
+    val weight: String? = null,
+    val lifestyle: String? = null,
+)
