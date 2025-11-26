d74a7cd4a0a33c9590d7a8f8a711f7c8a6be0205
RenHeyzer <vesolyyradin@gmail.com>
2025-11-26 22:11:13 +0600

    implement registration

--- app/src/main/java/com/azim/fitness/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/azim/fitness/ui/registration/RegistrationFragment.kt
@@ -1,18 +1,125 @@
 package com.azim.fitness.ui.registration
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.azim.fitness.R
+import com.azim.fitness.container
+import com.azim.fitness.databinding.FragmentRegistrationBinding
+import com.azim.fitness.db.entity.Lifestyle
+import com.azim.fitness.db.entity.UserEntity
+import com.azim.fitness.utils.UIState
+import kotlinx.coroutines.launch
 
 class RegistrationFragment : Fragment() {
+
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<RegistrationViewModel> {
+        RegistrationViewModelFactory(userRepository = requireContext().container.userRepository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+    ): View {
+        _binding = FragmentRegistrationBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        incrementAndDecrementAge()
+        onRegisterButtonClick()
+        observeState()
+    }
+
+    private fun initAdapter() {
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            listOf(getString(R.string.sedentary_lifestyle), getString(R.string.active_lifestyle))
+        )
+        binding.actvLifestyle.setAdapter(adapter)
+    }
+
+    private fun incrementAndDecrementAge() = with(binding) {
+        btnPlus.setOnClickListener {
+            var age = etAge.text.toString().trim().toInt()
+            if (age <= 120) {
+                age++
+                etAge.setText(age.toString())
+            }
+        }
+        btnMinus.setOnClickListener {
+            var age = etAge.text.toString().trim().toInt()
+            if (age > 0) {
+                age--
+                etAge.setText(age.toString())
+            }
+        }
+    }
+
+    private fun onRegisterButtonClick() = with(binding) {
+        btnRegister.setOnClickListener {
+            val lastname = etSurname.text.toString().trim()
+            val firstname = etFirstname.text.toString().trim()
+            val age = etAge.text.toString().trim().toInt()
+            val weight = etWeight.text.toString().trim().toFloat()
+            val height = etHeight.text.toString().trim().toFloat()
+            val email = etEmail.text.toString().trim()
+            val lifestyle = actvLifestyle.text.toString()
+
+            val user = UserEntity(
+                lastname = lastname,
+                firstname = firstname,
+                age = age,
+                weight = weight,
+                height = height,
+                email = email,
+                lifestyle = if (lifestyle == Lifestyle.SEDENTERY.value)
+                    Lifestyle.SEDENTERY else Lifestyle.ACTIIVE
+            )
+            viewModel.register(user, lifestyle)
+        }
+    }
+
+    private fun observeState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.observe(viewLifecycleOwner) {
+                    when (it) {
+                        is UIState.Error<Messages> -> with(binding) {
+                            tilSurname.error = it.error.lastname
+                            tilFirstname.error = it.error.firstname
+                            tilAge.error = it.error.age
+                            tilHeight.error = it.error.height
+                            tilWeight.error = it.error.weight
+                            tilEmail.error = it.error.email
+                            tilLifestyle.error = it.error.lifestyle
+                        }
+                        UIState.Loading -> {}
+                        is UIState.Success<Long> -> {
+                            Log.e("success", it.data.toString())
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/azim/fitness/ui/registration/RegistrationViewModel.kt
@@ -0,0 +1,79 @@
+package com.azim.fitness.ui.registration
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.azim.fitness.data.repository.UserRepository
+import com.azim.fitness.db.entity.UserEntity
+import com.azim.fitness.utils.UIState
+import kotlinx.coroutines.launch
+
+class RegistrationViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    private val _uiState = MutableLiveData<UIState<Long, Messages>>(UIState.Loading)
+    val uiState: LiveData<UIState<Long, Messages>> = _uiState
+
+    fun register(user: UserEntity, lifestyle: String) {
+        val messages = validateUserInfo(user, lifestyle)
+        if (messages.hashCode() == 0) {
+            viewModelScope.launch {
+                val result = userRepository.addUser(user)
+                _uiState.value = UIState.Success(result)
+            }
+        } else {
+            _uiState.value = UIState.Error(messages)
+        }
+    }
+
+    fun validateUserInfo(user: UserEntity, lifestyle: String): Messages {
+        with(user) {
+            val lastnameMessage =
+                if (lastname.isBlank()) "Заполните поле!"
+                else null
+            val firstnameMessage =
+                if (firstname.isBlank()) "Заполните поле!"
+                else null
+            val regex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
+            val emailMessage =
+                if (!regex.matches(email)) "Неверный формат для Email"
+                else null
+            val ageMessage =
+                if (age !in 0..120) "Недопустимое значение возраста"
+                else null
+            val heightMessage =
+                if (height !in 100.0f..260.0f) "Недопустимое значение роста"
+                else null
+            val weightMessage =
+                if (weight !in 10.0f..200.0f) "Недопустимое значение веса"
+                else null
+            val lifestyleMessage =
+                if (lifestyle.isBlank()) "Выберите хотя бы одно значение"
+                else null
+
+            val messages = Messages(
+                lastname = lastnameMessage,
+                firstname = firstnameMessage,
+                email = emailMessage,
+                age = ageMessage,
+                height = heightMessage,
+                weight = weightMessage,
+                lifestyle = lifestyleMessage
+            )
+            return messages
+        }
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class RegistrationViewModelFactory(
+    private val userRepository: UserRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RegistrationViewModel::class.java)) {
+            return RegistrationViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
