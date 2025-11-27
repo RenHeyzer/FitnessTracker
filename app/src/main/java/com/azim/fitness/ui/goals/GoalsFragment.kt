d5c95b673fa95683fd01d79479bf9a5964e7fda9
RenHeyzer <vesolyyradin@gmail.com>
2025-11-27 18:33:55 +0600

    implement GoalsFragment

--- app/src/main/java/com/azim/fitness/ui/goals/GoalsFragment.kt
+++ app/src/main/java/com/azim/fitness/ui/goals/GoalsFragment.kt
@@ -5,14 +5,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.azim.fitness.R
+import com.azim.fitness.databinding.FragmentGoalsBinding
 
 class GoalsFragment : Fragment() {
+
+    private var _binding: FragmentGoalsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_goals, container, false)
+    ): View {
+        _binding = FragmentGoalsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onGoalSelect()
+    }
+
+    private fun onGoalSelect() = with(binding) {
+        toggleGroupGoals.addOnButtonCheckedListener { _, _, isChecked ->
+            if (isChecked) {
+                btnContinue.isEnabled = true
+            }
+        }
+        btnContinue.setOnClickListener {
+            when (toggleGroupGoals.checkedButtonId) {
+                R.id.btn_goal_lose_weight ->
+                    findNavController().navigate(
+                        GoalsFragmentDirections.actionGoalsFragmentToGoalsSecondFragment(1)
+                    )
+                R.id.btn_goal_gain_muscle ->
+                    findNavController().navigate(
+                        GoalsFragmentDirections.actionGoalsFragmentToGoalsSecondFragment(2)
+                    )
+                R.id.btn_goal_gain_weight ->
+                    findNavController().navigate(
+                        GoalsFragmentDirections.actionGoalsFragmentToGoalsSecondFragment(3)
+                    )
+                R.id.btn_goal_maintain_form ->
+                    findNavController().navigate(
+                        GoalsFragmentDirections.actionGoalsFragmentToGoalsSecondFragment(4)
+                    )
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
