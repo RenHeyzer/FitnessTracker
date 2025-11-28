ff946665d617aecd615eace8e536f35d57c4c43b
RenHeyzer <vesolyyradin@gmail.com>
2025-11-28 13:43:52 +0600

    implement GoalsSecondFragment

--- app/src/main/java/com/azim/fitness/ui/goals/GoalsSecondFragment.kt
+++ app/src/main/java/com/azim/fitness/ui/goals/GoalsSecondFragment.kt
@@ -5,14 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.azim.fitness.R
+import com.azim.fitness.container
+import com.azim.fitness.databinding.FragmentGoalsSecondBinding
 
 class GoalsSecondFragment : Fragment() {
+
+    private var _binding: FragmentGoalsSecondBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_goals_second, container, false)
+    ): View {
+        _binding = FragmentGoalsSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        defineGoal()
+        saveTargetWeight()
+    }
+
+    private fun defineGoal() {
+        when (requireContext().container.preferencesHelper.goal) {
+            Goal.LOOSE_WEIGHT -> binding.tvSubtitleGoal.text =
+                getString(R.string.how_many_kg_lose_text)
+
+            Goal.GAIN_WEIGHT -> binding.tvSubtitleGoal.text =
+                getString(R.string.how_many_kg_gain_text)
+
+            else -> {}
+        }
+    }
+
+    private fun saveTargetWeight() {
+        binding.btnContinueWeight.setOnClickListener {
+            val value = binding.etTargetWeight.text.toString().trim().toFloat()
+            requireContext().container.preferencesHelper.apply {
+                targetWeight = value
+                isGoalsDefined = true
+            }
+            findNavController().navigate(R.id.action_goalsSecondFragment_to_mainFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
