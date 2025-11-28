b9a5c35687b38aa345b2338052ce3a46118d3db8
RenHeyzer <vesolyyradin@gmail.com>
2025-11-28 13:44:21 +0600

    add goal and targetWeight properties

--- app/src/main/java/com/azim/fitness/preferences/PreferencesHelper.kt
+++ app/src/main/java/com/azim/fitness/preferences/PreferencesHelper.kt
@@ -1,6 +1,7 @@
 package com.azim.fitness.preferences
 
 import android.content.Context
+import com.azim.fitness.ui.goals.Goal
 
 class PreferencesHelper(context: Context) {
 
@@ -15,9 +16,22 @@ class PreferencesHelper(context: Context) {
         set(value) = sharedPreferences.edit().putBoolean(GOALS_DEFINED_KEY, value).apply()
         get() = sharedPreferences.getBoolean(GOALS_DEFINED_KEY, false)
 
+    var goal: Goal
+        set(value) = sharedPreferences.edit().putString(GOAL_KEY, value.name).apply()
+        get() {
+            val value = sharedPreferences.getString(GOAL_KEY, Goal.LOOSE_WEIGHT.name)
+            return Goal.valueOf(value.toString())
+        }
+
+    var targetWeight: Float
+        set(value) = sharedPreferences.edit().putFloat(TARGET_WEIGHT_KEY, value).apply()
+        get() = sharedPreferences.getFloat(TARGET_WEIGHT_KEY, 0.0f)
+
     companion object {
         private const val PREFERENCES_NAME = "preferences"
         const val AUTHORIZED_KEY = "authorized"
         const val GOALS_DEFINED_KEY = "goals"
+        const val GOAL_KEY = "goal"
+        const val TARGET_WEIGHT_KEY = "target_weight"
     }
 }
