048ef17898d321890f92c8a2135654749141461a
RenHeyzer <vesolyyradin@gmail.com>
2025-11-29 04:11:37 +0600

    complete weight dialog

--- app/src/main/java/com/azim/fitness/ui/main/TodaysWeightDialog.kt
+++ app/src/main/java/com/azim/fitness/ui/main/TodaysWeightDialog.kt
@@ -1,4 +1,55 @@
 package com.azim.fitness.ui.main
 
-class TodaysWeightDialog {
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.azim.fitness.R
+import com.azim.fitness.databinding.DialogTodaysWeightBinding
+
+class TodaysWeightDialog : DialogFragment() {
+
+    private var _binding: DialogTodaysWeightBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        _binding = DialogTodaysWeightBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSaveWeight.setOnClickListener {
+            val currentWeight = binding.etCurrentWeight.text.toString().trim()
+            if (currentWeight.isNotBlank() && currentWeight != "0.0" && currentWeight != "0") {
+                findNavController().apply {
+                    previousBackStackEntry?.savedStateHandle?.set(
+                        WEIGHT_KEY,
+                        currentWeight.toFloat()
+                    )
+                    popBackStack()
+                }
+            } else {
+                binding.tilCurrentWeight.error = getString(R.string.input_current_weight)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val WEIGHT_KEY = "weight"
+    }
 }
